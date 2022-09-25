5a003f6bf77ac5c42f9797f19ac469d23dbb5ad9
Thyago Silveira <83200721+Thyaguixx@users.noreply.github.com>
2022-09-25 12:13:44 -0300

    Add files via upload

---
+++ 2ª Sprint/bancoAPI.sql
@@ -0,0 +1,30 @@
+create database API_LittleTech;
+use API_LittleTech;
+
+create table usuario(
+	id_usuario int not null primary key AUTO_INCREMENT,
+    nome_usuario varchar(50) not null,
+    email_usuario varchar(50) not null,
+    username_usuario varchar(50) not null,
+    senha_usuario varchar(50) not null
+    );
+
+create table canais(
+	id_canal int not null primary key AUTO_INCREMENT,
+    nome_canal varchar(50) not null,
+    tipo_canal varchar(50) not null,
+    padrao_autenticacao varchar(50) not null
+    );
+
+create table configuracao_canais(
+	id_canal int not null primary key AUTO_INCREMENT,
+    nome_canal varchar(50) null,
+    id_conta varchar(50) null,
+    token_canal varchar(50) null,
+    usuario_canal varchar(50) null,
+    senha_canal varchar(50) null
+    );
+    
+select * from usuario;
+select * from canais;
+select * from configuracao_canais;
